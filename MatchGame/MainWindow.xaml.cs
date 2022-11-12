80afb1942cd1193d38d1b045eda4ca7dc1f89ef5
Yore <724360795@qq.com>
2022-11-12 19:54:31 +0800

    Bug fixed

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -66,6 +66,7 @@ namespace MatchGame
             {
                 if (textBlock.Name != "timeTextBlock") 
                 {
+                    textBlock.Visibility= Visibility.Visible;
                     int index = random.Next(animalEmoji.Count);
                     string nextEmoji = animalEmoji[index];
                     textBlock.Text = nextEmoji;
